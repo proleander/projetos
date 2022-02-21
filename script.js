17ba8d1f9b623f0324627954f975173f4e272525
Leandro <leandro.tisistemas@gmail.com>
2022-02-21 12:12:53 -0300

    Aplicação estaval sem local storage

---
+++ script.js
@@ -0,0 +1,101 @@
+const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount =document.querySelector('#amount')
+
+//console.log({inputTransactionName, inputTransactionAmount})
+/* console.log({ incomeDisplay, expenseDisplay, balanceDisplay}) */
+
+let dummyTransactions = [
+    { id: 1, name: 'Bolo de Brigadeiro', amount: -20 },
+    { id: 2, name: 'Salário', amount: 300 },
+    { id: 3, name: 'Torta de Frango', amount: -10 },
+    { id: 4, name: 'Violão', amount: 150 }
+]
+
+    const removeTransaction = ID => {
+
+        dummyTransactions = dummyTransactions.filter(transaction => transaction.id !== ID)
+        init()
+    }
+
+const addTransactionIntoDOM = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+    const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+    const amountWithoutOperator = Math.abs(transaction.amount)
+    const li = document.createElement('li')
+
+    li.classList.add(CSSClass)
+    li.innerHTML = `
+     ${transaction.name} <span>${operator} R$ ${amountWithoutOperator}
+     </span>
+     <button class="delete-btn" onClick="removeTransaction(${transaction.id})">
+     x
+     </button>
+    `
+    transactionsUl.prepend(li)
+    
+}
+
+const updateBalanceValues = () => {
+    const transactionsAmounts = dummyTransactions
+    .map(transaction => transaction.amount)
+    const total = transactionsAmounts
+    .reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2)
+    const income = transactionsAmounts
+    .filter(value => value > 0)
+    .reduce((accumulator, value) => accumulator + value, 0)
+    .toFixed(2)
+//console.log(income)
+
+
+const expense = Math.abs(transactionsAmounts
+    .filter(value => value < 0)
+    .reduce((accumulator, value) => accumulator + value, 0))
+    .toFixed(2)   
+  //  console.log(expense)
+
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
+
+}
+
+const init = () => {
+    transactionsUl.innerHTML = ''
+    dummyTransactions.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
+}
+
+init()
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const transactionName = inputTransactionName.value.trim()
+    const transactionsAmount = inputTransactionAmount.value.trim() 
+
+    if (transactionName === '' || transactionsAmount === '') {
+        alert('Por favor Preencha tanto o nome quanto o valor da transação')
+        return
+    }
+    const transaction = { 
+        id: generateID(), 
+        name: transactionName, 
+        amount: +transactionsAmount 
+    }
+    dummyTransactions.push(transaction)
+    init()
+
+    inputTransactionName.value = ''
+    inputTransactionAmount.value = ''
+    
+        //console.log(transaction)
+})
+
+//addTransactionIntoDOM(dummyTransactions[0])
